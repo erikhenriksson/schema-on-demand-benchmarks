0f69bce2df4c82f733ea5f2e175f07fc5c0bd0cb
Erik <eekihenriksson@gmail.com>
2025-09-03 17:38:30 +0300

    Update download_datasets.py

--- download_datasets.py
+++ download_datasets.py
@@ -1,11 +1,36 @@
 from huggingface_hub import snapshot_download
 
 snapshot_download(
-    repo_id="stanfordnlp/imdb", repo_type="dataset", local_dir="./raw/imdb"
+    repo_id="stanfordnlp/imdb",
+    repo_type="dataset",
+    local_dir="./raw/imdb",
+    cache_dir="./cache/",
 )
 
 snapshot_download(
     repo_id="McAuley-Lab/Amazon-Reviews-2023",
     repo_type="dataset",
     local_dir="./raw/amazon",
+    cache_dir="./cache/",
+)
+
+snapshot_download(
+    repo_id="fancyzhx/ag_news",
+    repo_type="dataset",
+    local_dir="./raw/ag_news",
+    cache_dir="./cache/",
+)
+
+snapshot_download(
+    repo_id="fancyzhx/SetFit/bbc-news",
+    repo_type="dataset",
+    local_dir="./raw/bbc_news",
+    cache_dir="./cache/",
+)
+
+snapshot_download(
+    repo_id="tasksource/jigsaw_toxicity",
+    repo_type="dataset",
+    local_dir="./raw/jigsaw_toxicity",
+    cache_dir="./cache/",
 )
